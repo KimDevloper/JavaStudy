aa26b466d158c72b7388fb1cf9abcca0c3730c02
KimDevloper <a62387272@gmail.com>
2023-07-07 20:26:29 +0900

    [ADD] 명품자바 Ch01 실습예제 추가

---
+++ src/Exercise/Project1_1.java
@@ -0,0 +1,9 @@
+package Exercise;
+
+public class Project1_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome !!");
+        System.out.println("자바 세계로 오신것을 환영합니다.");
+    }
+
+}
---
+++ src/Exercise/Project1_2.java
@@ -0,0 +1,8 @@
+package Exercise;
+
+public class Project1_2 {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌이서 미안합니다.");
+    }
+}
