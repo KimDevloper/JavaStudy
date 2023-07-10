207965b8f81610205022e259c6ee4665b9d93c96
KimDevloper <a62387272@gmail.com>
2023-07-10 18:37:44 +0900

    [!HOTFIX] TV 상속하는 예제 ColorTV 코드 수정

--- src/ch05/extends03/ColorTV.java
+++ src/ch05/extends03/ColorTV.java
@@ -2,6 +2,9 @@ package ch05.extends03;
 
 public class ColorTV extends TV {
     private String Color;
+    public ColorTV() {
+
+    }
     public ColorTV(int size, int buyingData, String Color) {
         super(size, buyingData);
         this.Color = Color;
--- src/ch05/extends03/TV_Test.java
+++ src/ch05/extends03/TV_Test.java
@@ -8,11 +8,11 @@ public class TV_Test {
         tv1.setBuyingDate(220708);
        // tv1.showInfo();
 
-        IPTV iptv = new IPTV();
+        /*IPTV iptv = new IPTV();
         iptv.setSize(55);
         iptv.setBuyingDate(221028);
         iptv.setColor("실버");
         iptv.setIpAddr("192.168.10.123");
-        iptv.showInfo();
+        iptv.showInfo(); */
     }
 }
